d77d5f3ca2afded24c25b4905e2926c9e6363037
Daniel Kukula <daniel.kuku@gmail.com>
2023-01-28 10:29:08 +0000

    initial commit

---
+++ lib/logger_error.ex
@@ -0,0 +1,18 @@
+defmodule LoggerError do
+  @moduledoc """
+  Documentation for `LoggerError`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoggerError.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/logger_error_test.exs
@@ -0,0 +1,8 @@
+defmodule LoggerErrorTest do
+  use ExUnit.Case
+  doctest LoggerError
+
+  test "greets the world" do
+    assert LoggerError.hello() == :world
+  end
+end
