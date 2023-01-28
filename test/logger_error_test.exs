c8a70d8264b6f2f95d705baa97a8b3cd862c546e
Daniel Kukula <daniel.kuku@gmail.com>
2023-01-28 10:31:19 +0000

    changes with bug

--- test/logger_error_test.exs
+++ test/logger_error_test.exs
@@ -3,6 +3,10 @@ defmodule LoggerErrorTest do
   doctest LoggerError
 
   test "greets the world" do
+    Logger.App.stop()
+    Logger.App.start()
     assert LoggerError.hello() == :world
+    assert LoggerError.hello() != :world
+    refute LoggerError.hello() == :world
   end
 end
